5bd65e61828f60b2e1f590c7c8b115ca7120aa1b
Deathlymad <meyerphilip96@googlemail.com>
2023-04-30 15:29:38 +0200

    updated lighting a bit. made testTree Collidable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 0.02f;
+    public float rotSpeed = 0.02f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,19 +18,27 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey("w"))
         {
-            transform.position += transform.forward * moveSpeed;
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey("a"))
         {
-            transform.position -= transform.right * moveSpeed;
+            transform.position -= transform.right * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey("s"))
         {
-            transform.position -= transform.forward * moveSpeed;
+            transform.position -= transform.forward * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey("d"))
         {
-            transform.position += transform.right * moveSpeed;
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.rotation *= Quaternion.AngleAxis(-rotSpeed*Time.deltaTime, Vector3.up);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.rotation *= Quaternion.AngleAxis(rotSpeed * Time.deltaTime, Vector3.up);
         }
     }
 
