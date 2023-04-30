68f4ac7ff338c0eefaeab0cd32b848ef200372e3
Deathlymad <meyerphilip96@googlemail.com>
2023-04-30 19:09:37 +0200

    added basics for pickup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,12 @@ public class Player : MonoBehaviour
     public Animator playerAnim;
 
     private bool canMove = true;
+    private GameObject item = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void enterCity()
@@ -23,42 +25,70 @@ public class Player : MonoBehaviour
     {
         canMove = true;
     }
+    public void nearItem(GameObject itm)
+    {
+        item = itm;
+    }
+    public void leaveItem(GameObject itm)
+    {
+        if (item == itm)
+            item = null;
+    }
+
+    public OnPickup()
+    {
+        //TODO add to inventory
+        obj.leaveItem(obj);
+    }
 
     // Update is called once per frame
     void Update()
     {
         bool isDown = false;
+        float turnFactor = 1.0f;
         if (canMove)
         {
+            if (Input.GetAxis("Mouse X") > 0.01 || Input.GetAxis("Mouse X") < -0.01)
+            {
+                transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime, Vector3.up);
+                isDown |= true;
+                turnFactor = 0.8f;
+            }
+            else
+            {
+                turnFactor = 1.0f;
+            }
             if (Input.GetKey("w"))
             {
-                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+                transform.position += transform.forward * turnFactor * moveSpeed * Time.deltaTime;
                 isDown |= true;
             }
             if (Input.GetKey("a"))
             {
-                transform.position -= transform.right * moveSpeed * Time.deltaTime;
+                transform.position -= transform.right * turnFactor * moveSpeed * Time.deltaTime;
                 isDown |= true;
             }
             if (Input.GetKey("s"))
             {
-                transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+                transform.position -= transform.forward * turnFactor * moveSpeed * Time.deltaTime;
                 isDown |= true;
             }
             if (Input.GetKey("d"))
             {
-                transform.position += transform.right * moveSpeed * Time.deltaTime;
+                transform.position += transform.right * turnFactor * moveSpeed * Time.deltaTime;
                 isDown |= true;
             }
-            if (Input.GetKey("q"))
-            {
-                transform.rotation *= Quaternion.AngleAxis(-rotSpeed * Time.deltaTime, Vector3.up);
-                isDown |= true;
-            }
+
+
             if (Input.GetKey("e"))
             {
-                transform.rotation *= Quaternion.AngleAxis(rotSpeed * Time.deltaTime, Vector3.up);
-                isDown |= true;
+                if (item != null)
+                {
+                    item.GetComponen<Item>().pickUp(this);
+                    playerAnim.ResetTrigger("Running");
+                    playerAnim.SetTrigger("StopRunning");
+                    playerAnim.SetTrigger("Pickup");
+                }
             }
         }
         if (Input.GetKey("x"))
