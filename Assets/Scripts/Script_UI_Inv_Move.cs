9d7b3eb9c2349b383a34284381db64428fa35b88
LordBlackheart <82619092+LordBlackheart@users.noreply.github.com>
2023-04-30 16:16:42 +0200

    addedsome ui elemnts

---
+++ Assets/Scripts/Script_UI_Inv_Move.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_UI_Inv_Move : MonoBehaviour
+{
+    public GameObject _Panel;
+    private Vector3 _Current_Position;
+    private bool _Display;
+    private float _Move_Panel_Max = 200f;
+    // Use this for initialization
+    void Start()
+    {
+        //Starting Positon
+        _Current_Position = _Panel.transform.position;
+        _Display = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Panel.transform.position = _Current_Position;
+    }
+
+    public void Panel_Click()
+    {
+        if (_Display)
+        {
+            //Hide Panel
+            _Display = false;
+            _Current_Position.x -= _Move_Panel_Max;
+        }
+        else
+        {
+            //Display Panel
+            _Display = true;
+            _Current_Position.x += _Move_Panel_Max;
+        }
+    }
+}
