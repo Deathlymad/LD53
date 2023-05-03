b4513c6fa0cef24ccd4363c9b7d2ebcd17a9b177
Deathlymad <meyerphilip96@googlemail.com>
2023-05-03 17:11:20 +0200

    updated build to not be as stupid and update the stuff

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,7 +7,7 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene("Scenes/" + sceneName);
         Debug.Log("Loading: " + sceneName);
     }
 }
